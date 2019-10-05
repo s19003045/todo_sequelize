ad33fca86235ab3d580357f7731466a51389f986
Gary Wu <s19003045@gmail.com>
2019-10-05 11:46:55 +0800

    Add OAuth Facebook login feature

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
+
+// 判別開發環境
+if (process.env.NODE_ENV !== 'production') {      // 如果不是 production 模式
+  require('dotenv').config()                      // 使用 dotenv 讀取 .env 檔案
+}
+
 const port = 3000
 const methodOverride = require('method-override')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+
+
 
 // 載入 user model
 const db = require('./models')
@@ -56,6 +64,8 @@ app.use('/', require('./routes/home'))
 app.use('/todos', require('./routes/todo'))
 
 app.use('/users', require('./routes/user'))
+
+app.use('/auth', require('./routes/auth'))
 
 
 // Server start
--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 
 const LocalStrategy = require('passport-local')
-
+const FacebookStrategy = require('passport-facebook').Strategy
 
 const db = require('../models')
 const User = db.User
@@ -29,6 +29,56 @@ module.exports = passport => {
           })
       })
     })
+  )
+
+
+  // 使用 passport-facebook
+  passport.use(
+
+    new FacebookStrategy({
+      clientID: process.env.FACEBOOK_ID, // app ID
+      clientSecret: process.env.FACEBOOK_SECRET,  //app secret
+      callbackURL: process.env.FACEBOOK_CALLBACK,  // facebook authorization server 
+      profileFields: ['email', 'displayName'] //使用者提供的資料範圍： email 及 displayName
+    }, (accessToken, refreshToken, profile, done) => {
+
+      // 可以透過下面程式碼得知 expense-tracker server 拿到什麼資料： console.log(profile)
+      console.log(profile)
+
+      // expense-tracker API server 向自己的 mongoDB 資料庫確認是否已有人已此 email 建立帳號 
+      User.findOne({ where: { email: profile._json.email } }
+      ).then(user => {
+        // 如果 email 不存在就建立新的使用者
+        if (!user) {
+          const newUser = new User({
+            name: profile._json.name,
+            email: profile._json.email,
+            password: ''
+          })
+          let randomPassword = Math.random().toString().slice(-8)
+
+          bcrypt.genSalt(10, (err, salt) => {
+            // if (err) return console.error(err)
+            bcrypt.hash(randomPassword, salt, (err, hash) => {
+              if (err) throw err //throw err 執行後，會直接忽略緊接在後面的程式碼
+              // 加密過程如果有問題，就不會在 mongoDB 裡面新增 newUser
+              newUser.password = hash
+              newUser.save()
+                .then((err, user) => {
+                  if (err) return console.error(err)
+                  return res.redirect('/')
+                })
+                .catch(err => console.log(err))
+            })
+          })
+        }
+        // 當 mongoDB 資料庫已有此 email，就不需在資料庫新增此 email 的資料。這代表使用者之前在 expense-tracker server 就已透過第三方認證登入網站了！
+        else {
+          return done(null, user)
+        }
+      })
+    }
+    )
   )
 
 
